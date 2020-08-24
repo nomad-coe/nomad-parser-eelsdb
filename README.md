0d67544ea06713b0cf333eeee8d6ef6929a47f5e
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-08-24 18:55:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NOMAD EELSDB crawler/converter
+
+This project contains code to retrieve EELS data and add it to NOMAD.
---
+++ eelsdbconverter/__init__.py
@@ -0,0 +1,98 @@
+# Copyright 2016-2018 Markus Scheidgen
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import sys
+import os.path
+import json
+import ase
+import numpy as np
+from datetime import datetime
+import time
+import ast
+import logging
+import re
+
+from .metainfo import eels as meels
+from nomad.datamodel.metainfo.general_experimental import (
+    section_experiment as Experiment,
+    section_sample as Sample,
+    section_method as Method,
+    section_data as Data)
+from nomad.parsing.parser import FairdiParser
+
+
+logger = logging.getLogger(__name__)
+
+
+class EELSApiJsonConverter(FairdiParser):
+    def __init__(self):
+        super().__init__(
+            name='parsers/eelsdb', code_name='eels', code_homepage='https://eelsdb.eu/',
+            domain='ems',
+            mainfile_mime_re=r'application/json',
+            mainfile_contents_re=(r'https://eelsdb.eu/spectra')
+        )
+
+    def parse(self, filepath, archive, logger=logger):
+        with open(filepath) as f:
+            data = json.load(f)
+
+        experiment = archive.m_create(Experiment)
+        experiment.raw_metadata = data
+
+        experiment.experiment_publish_time = datetime.strptime(
+            data.get('published'), '%Y-%m-%d %H:%M:%S')
+
+        sample = experiment.m_create(Sample)
+        sample.sample_chemical_formula = data.get('formula')
+        sample.sample_atom_labels = data.get('elements')
+        sample.sample_chemical_name = data.get('title')
+
+        experiment.m_create(
+            Method,
+            experiment_method_name='electron energy loss spectroscopy',
+            experiment_method_abbreviation='EELS')
+
+        section_data = experiment.m_create(Data)
+
+        archive.section_metadata.external_id = str(data.get('id'))
+        archive.section_metadata.external_db = 'EELSDB'
+
+        author = data.get('author')['name']
+        author = re.sub(r'\(.*\)', '', author).strip()
+        archive.section_metadata.coauthors = [dict(
+            first_name=' '.join(author.split(' ')[0:-1]),
+            last_name=author.split(' ')[-1])]
+        archive.section_metadata.comment = data.get('description')
+        archive.section_metadata.references = [
+            data.get('permalink'),
+            data.get('api_permalink'),
+            data.get('download_link')
+        ]
+
+        reference = data.get('reference')
+        if reference:
+            if isinstance(reference, str):
+                archive.section_metadata.references.append(reference)
+            elif isinstance(reference, dict):
+                if 'freetext' in reference:
+                    archive.section_metadata.references.append(
+                        re.sub(r'\r+\n+', '; ', reference['freetext']))
+                else:
+                    archive.section_metadata.references.append('; '.join([
+                        str(value).strip() for value in reference.values()
+                    ]))
+
+        section_data.data_repository_name = 'Electron Energy Loss Spectroscopy (EELS) database'
+        section_data.data_repository_url = 'https://eelsdb.eu'
+        section_data.entry_repository_url = data.get('permalink')
